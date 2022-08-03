c94032e2fbef106a6537b8b5961fdb7a057ad8e9
youssef67 <you.moudni@gmail.com>
2022-08-03 22:08:18 +0200

    test commit

--- delete-article.php
+++ delete-article.php
@@ -6,6 +6,7 @@ $_GET = filter_input_array(INPUT_GET, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 $id = $_GET['id'] ?? "";
 
 
+
 if ($id) {
 
     $articles = json_decode(file_get_contents($filename), true);
